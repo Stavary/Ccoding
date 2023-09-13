7c34516b25668b2cf62234082d7687894b95c4f9
stavary wang <stavary@bupt.edu.cn>
2023-09-13 16:59:51 +0800

    9.13.4

--- C语言.c
+++ C语言.c
@@ -1,7 +1,19 @@
 #include<stdio.h>
+int MAX(int x,int y)
+{
+   if(x<=y)
+    return y;
+   else 
+    return x;
+}     
 int main()
 {
-    int a;
-    int b;
+    int a,b,c,d,e;
+    a=0;b=0;c=0;d=0;e=0;
+    printf("输入三个整数：");
+    scanf("%d %d %d\n",&a,&b,&c); 
+    d=MAX(a,b);
+    e=MAX(c,d);
+    printf("最大的数是：%d\n",e);
     return 0;
 }
